7f6f358587372ab0912753d0d26212db1388326d
SrJohn369 <srjohn963@gmail.com>
2024-05-23 01:51:03 -0300

    Moedas fortes e fracas

--- src/main/java/br/com/conversor/cotacao/CotacaoAtual.java
+++ src/main/java/br/com/conversor/cotacao/CotacaoAtual.java
@@ -4,22 +4,22 @@ import com.google.gson.*;
 
 import br.com.conversor.conectors.ConectorExchangeRate;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CotacaoAtual extends ConectorExchangeRate {
     // ATRIBUTOS --------------------
-    private final String moeda;
+    private final String moedaBase;
 
     // CONSTRUTOR --------------------
-    public CotacaoAtual(String moeda) {
-        this.moeda = moeda;
+    public CotacaoAtual(String moedaBase) {
+        this.moedaBase = moedaBase;
     }
 
     // MÉTODOS ------------------------
-    public Double valorCotaContra(String outraMoeda) {
-        ConectorExchangeRate conn = new ConectorExchangeRate(moeda, outraMoeda);
-        System.out.println(conn);
+    public Double valorCotaContra(String moedaCotacao) {
+        ConectorExchangeRate conn = new ConectorExchangeRate(moedaBase, moedaCotacao);
 
         // Capturar o json da request
         String jsonString = conn.respostaAPI().body();
@@ -29,6 +29,43 @@ public class CotacaoAtual extends ConectorExchangeRate {
         JsonObject objJson = elementoJson.getAsJsonObject();
 
         return objJson.get("conversion_rate").getAsDouble();
+    }
+
+    public Map<String, Double> mapeia(ConectorExchangeRate conn) {
+        // Capturar o json da request
+        String jsonString = conn.respostaAPI().body();
+        // Transformando num elemento
+        JsonElement elementoJson = JsonParser.parseString(jsonString);
+        // Transformar num obj
+        JsonObject objJson = elementoJson.getAsJsonObject();
+        // Pega o array dentro do Json
+        JsonObject conversionRates = objJson.getAsJsonObject("conversion_rates");
+
+        // Convertendo the JsonObject to a Map
+        Map<String, Double> ratesMap = new HashMap<>();
+        for (String key : conversionRates.keySet()) {
+            ratesMap.put(key, conversionRates.get(key).getAsDouble());
+        }
+
+        return ratesMap;
+    }
+
+    // Forte contra
+    public Map.Entry<String, Double> forteContra(String moedaBase){
+        ConectorExchangeRate conn = new ConectorExchangeRate(moedaBase);
+
+        Map<String, Double> ratesMap = mapeia(conn);
+
+        return Collections.max(ratesMap.entrySet(), Map.Entry.comparingByValue());
+    }
+
+    // Fraco Contra
+    public Map.Entry<String, Double> fracoContra(String moedaBase){
+        ConectorExchangeRate conn = new ConectorExchangeRate(moedaBase);
+
+        Map<String, Double> ratesMap = mapeia(conn);
+
+        return Collections.min(ratesMap.entrySet(), Map.Entry.comparingByValue());
     }
 
 }
--- src/main/java/br/com/conversor/interacao/Menu.java
+++ src/main/java/br/com/conversor/interacao/Menu.java
@@ -4,9 +4,12 @@ import br.com.conversor.conectors.ConectorExchangeRate;
 import br.com.conversor.cotacao.CotacaoAtual;
 
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
 
 public final class Menu {
+    // inicar variáveis
+    Scanner leia = new Scanner(System.in);
 
     private ArrayList<Object> processaDados(int moedaBase, int moedaCotacao){
         // Vetor de identificação
@@ -21,6 +24,10 @@ public final class Menu {
         dadosProcessados.add(paridades[moedaBase]);
         dadosProcessados.add(paridades[moedaCotacao]);
         dadosProcessados.add(cota.valorCotaContra(paridades[moedaCotacao]));
+        dadosProcessados.add(cota.forteContra(paridades[moedaBase]).getKey());
+        dadosProcessados.add(cota.forteContra(paridades[moedaBase]).getValue());
+        dadosProcessados.add(cota.fracoContra(paridades[moedaBase]).getKey());
+        dadosProcessados.add(cota.fracoContra(paridades[moedaBase]).getValue());
 
         return dadosProcessados;
     }
@@ -40,35 +47,35 @@ public final class Menu {
                     =====================================================""");
     }
 
-    private void menuParidades(int moedaBase, int moedaCotacao) {
+    private double menuParidades(int moedaBase, int moedaCotacao) {
         ArrayList<Object> dadosProntos = processaDados(moedaBase, moedaCotacao);
 
         // Tela dinâmica
         System.out.printf("""
                     ==================CONVERSOR DE MOEDAS================
-                    |                                                   |
-                    | CONVERSOR %s/%s ---                               |
-                    | Para cada %s 1 -> %s %.2f                          |
-                    | ===== / =====                                     |
-                    | Mais FORTE contra: %s                             |
-                    | |--> Para cada R$ 1 -> %s %.2f                     |
-                    | mais FRACO contra: %s                             |
-                    |--> Para cada R$ 1 -> %s %.2f                       |
+                    |
+                    | CONVERSOR %s/%s ---
+                    | Para cada %s 1 -> %s %.2f
+                    | ===== / =====
+                    | Mais FORTE contra: %s
+                    | |--> Para cada %s 1 -> %s %.2f
+                    | mais FRACO contra: %s
+                    |--> Para cada %s 1 -> %s %.2f
                     =====================================================""",
                 dadosProntos.get(0),dadosProntos.get(1), dadosProntos.get(0),
-                dadosProntos.get(1), (double)dadosProntos.get(2),
-                "Abc", "fgh",3.55, "ijk", "lmn", 6.777
+                dadosProntos.get(1), (double)dadosProntos.get(2), dadosProntos.get(3),
+                dadosProntos.get(0), dadosProntos.get(3), (double)dadosProntos.get(4),
+                dadosProntos.get(5), dadosProntos.get(0), dadosProntos.get(5), (double)dadosProntos.get(6)
                 );
 
         // conversor
         System.out.printf("\nDigite o valor em %s: ", dadosProntos.get(0));
+        return leia.nextDouble();
     }
 
     public void inicia() {
         // MENU INTERATIVO
         while (true) {
-        // inicar variáveis
-        Scanner leia = new Scanner(System.in);
 
         // Escolhas
         menuInicio();
@@ -77,6 +84,7 @@ public final class Menu {
         System.out.println("=====================================================");
         //Finaliza o programa
         if (escolha == 99) {
+            leia.close();
             System.out.println("======  !!PROGRAMA FINALIZADO!!  =====");
             break;
         }
@@ -89,8 +97,10 @@ public final class Menu {
         switch (moedaBase) {
             // BRL BASE
             case 0:
-                menuParidades((moedaBase), moedaCotacao);
+                menuParidades(moedaBase, moedaCotacao);
+
             case 1:
+                menuParidades(moedaBase, moedaCotacao);
             case 2:
             case 3:
             case 4:
